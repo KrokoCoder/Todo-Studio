529bd14d5689f650bb688c89880cfda750cd91c2
Kroko Coder <137095983+KrokoCoder@users.noreply.github.com>
2024-02-10 09:11:41 +0000

    New Design of creationPlace.html Adding max limit of actions An opportunity to add actions. display:flex; Every HTML page was linked to it's Js file.

--- static/js/creationPlace.js
+++ static/js/creationPlace.js
@@ -1,13 +1,25 @@
+var limitofActions = 25;
+var numberOfActions = 0;
+
 function AddAction() {
-    var Action1 = document.getElementById("NewAction");
-    let textarea = document.createElement("textarea");
-    Action1.appendChild(textarea);
-    textarea.value = "Initial content";
-    textarea.setAttribute("cols", "50");
-    var addaddButton = document.createElement("button");
-    addaddButton.innerHTML = "Add";
-    addaddButton.setAttribute("id", "addButton");
-    Action1.appendChild(addaddButton);
+   
+        var Action1 = document.getElementById("NewAction");
+        let textarea = document.createElement("textarea");
+        Action1.appendChild(textarea);
+        textarea.setAttribute("id", "action");
+        textarea.value = "Initial content";
+        textarea.setAttribute("cols", "50");
+        var addaddButton = document.createElement("button");
+        addaddButton.innerHTML = "Add";
+        addaddButton.setAttribute("id", "addButton");
+        Action1.appendChild(addaddButton);
+        numberOfActions++;
+        
+    }
+    if (numberOfActions == limitofActions) {
+        var importantMessage = document.createElement("p");
+        importantMessage.innerHTML = "You have reached the limit of actions";
+        Action1.appendChild(importantMessage);
 }
 
 
