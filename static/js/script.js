15843194ed18a605c2c38083e4fcc4adad4c61dd
Kroko Coder <137095983+KrokoCoder@users.noreply.github.com>
2024-02-11 17:07:28 +0000

    More Css changes Switching buttons Improving NameUrTodo.html Faced with bug :(

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,3 @@
-var todoname;
-
-
-function ToDoName() {
-    var inputTodoName = document.getElementById("todoName"); // Renamed variable
-    var createTodo = document.getElementById("createTodo");
-    createTodo.addEventListener("click", function(e) {
-        e.preventDefault();
-
-        if (inputTodoName.value === "" || inputTodoName.value === null) {
-            console.log("Please enter a name for your todo.");
-        } else {
-            console.log("Todo name: " + inputTodoName.value);
-        }
-    });
-}
-
-
-
-
-window.onload = function() {
-    const todoNameInput = document.getElementById("todoName");
-    todoNameInput.addEventListener("click", ToDoName);
+function changename(){
+    document.getElementById('intextbox').name = "listingfields[1]";
 }
